0295d06e579fce1abbf34d4286ce7925209e8036
yeshushu <857438897@qq.com>
2017-02-21 10:17:10 +0800

    Update d.java

--- src/zzzz/d.java
+++ src/zzzz/d.java
@@ -2,7 +2,7 @@ package zzzz;
 
 public class d {
 	public  static void main(String args[]){
-<<<<<<< HEAD
+
 		 System.out.println("hello        world");
 		 System.out.println("hello        world"); 
 		 System.out.println("hello        world");
@@ -12,7 +12,7 @@ public class d {
 		
 		System.out.println("hello    world");
 	 
->>>>>>> branch 'master' of https://github.com/yeshushu/erp.git
+
 	}
 	
 }
